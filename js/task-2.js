a4931ddc83e7f6e9149cf1e38060bb31963b53c2
Mario Korzun <rkorzun2020@gmail.com>
2026-04-13 18:11:26 +0400

    feat: implement Storage class with methods for managing items

--- js/task-2.js
+++ js/task-2.js
@@ -8,8 +8,24 @@
 // getItems() — повертає масив поточних товарів у приватній властивості items.
 // addItem(newItem) — приймає новий товар newItem і додає його до масиву товарів у приватну властивість items об'єкта.
 // removeItem(itemToRemove) — приймає рядок з назвою товару itemToRemove і видаляє його з масиву товарів у приватній властивості items об'єкта.
+class Storage {
+  #items;
+  constructor(newItem) {
+    this.#items = newItem;
+  }
 
+  getItems() {
+    return this.#items;
+  }
+  addItem(newItem) {
+    return this.#items.push(newItem);
+  }
+  removeItem(itemToRemove) {
+    return (this.#items = this.#items.filter((item) => item !== itemToRemove));
+  }
+}
 const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+
 console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
 
 storage.addItem("Droid");
